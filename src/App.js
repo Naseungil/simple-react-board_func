4ae57d80b55584710e2c2bbdd4f307dc5547096f
Naseungil <sktmddlf505@naver.com>
2024-11-06 11:20:10 +0900

    보드리스트 함수 변경

--- src/App.js
+++ src/App.js
@@ -3,80 +3,67 @@ import "./App.css";
 import BoardList from "./BoardList";
 import Write from "./Write";
 import View from "./View";
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
-export default class App extends Component {
-  state = {
-    isModifyMode: false, //수정모드
-    isComplete: true, //렌더 완료(목록 출력 완료)
-    boardId: 0, //수정,삭제할 글 번호
-    redirect_to_write: false, //주소 변경 상태 추가
-    redirect_to_home: false,
-  };
+function App() {
+  const [isModifyMode, setIsModifyMode] = useState(false);
+  const [isComplete, setIsComplete] = useState(true);
+  const [boardId, setBoardId] = useState(0);
+  const [redirectToWrite, setRedirectToWrite] = useState(false);
+  const [redirectToHome, setRedirectToHome] = useState(false);
 
-  handleModify = (checkList) => {
+  const handleModify = (checkList) => {
     if (checkList.length === 0) {
       alert("수정할 게시글을 선택하세요");
     } else if (checkList.length > 1) {
       alert("하나의 게시글만 선택하세요");
+    } else {
+      setIsModifyMode(true);
+      setBoardId(checkList[0]);
+      setRedirectToWrite(true);
     }
-    this.setState({
-      isModifyMode: checkList.length === 1,
-      boardId: checkList[0] || 0,
-      redirect_to_write: true,
-    });
   };
 
-  handleCancel = () => {
-    this.setState({
-      isModifyMode: false,
-      isComplete: false,
-      boardId: 0,
-      redirect_to_home: true,
-    });
-    console.log("appjshandleCancel 실행");
+  const handleCancel = () => {
+    setIsModifyMode(false);
+    setIsComplete(false);
+    setBoardId(0);
+    setRedirectToHome(true);
   };
-  componentDidUpdate() {
-    // 리다이렉트 후 redirect 상태 초기화
-    if (this.state.redirect_to_write) {
-      this.setState({ redirect_to_write: false });
-    }
-    if (this.state.redirect_to_home) {
-      this.setState({ redirect_to_home: false });
-    }
-  }
-  render() {
-    return (
-      <BrowserRouter>
-        <div className="container">
-          <h1>React Board</h1>
-          {this.state.redirect_to_write && <Navigate to="/write" />}
-          {this.state.redirect_to_home && <Navigate to="/" />}
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <BoardList
-                  isComplete={this.state.isComplete}
-                  handleModify={this.handleModify}
-                />
-              }
-            />
-            <Route
-              path="/write"
-              element={
-                <Write
-                  isModifyMode={this.state.isModifyMode}
-                  boardId={this.state.boardId}
-                  handleCancel={this.handleCancel}
-                />
-              }
-            />
-            <Route path="/view" element={<View />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
-    );
-  }
+
+  useEffect(() => {
+    if (redirectToWrite) setRedirectToWrite(false);
+    if (redirectToHome) setRedirectToHome(false);
+  }, [redirectToWrite, redirectToHome]);
+
+  return (
+    <BrowserRouter>
+      <div className="container">
+        <h1>React Board</h1>
+        {redirectToWrite && <Navigate to="/write" />}
+        {redirectToHome && <Navigate to="/" />}
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <BoardList isComplete={isComplete} handleModify={handleModify} />
+            }
+          />
+          <Route
+            path="/write"
+            element={
+              <Write
+                isModifyMode={isModifyMode}
+                boardId={boardId}
+                handleCancel={handleCancel}
+              />
+            }
+          />
+          <Route path="/view/:id" element={<View />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
+export default App;
--- src/BoardList.js
+++ src/BoardList.js
@@ -1,114 +1,83 @@
-import React, { Component } from "react";
+import React, { Component, useCallback, useEffect, useState } from "react";
 import Table from "react-bootstrap/Table";
 import Button from "react-bootstrap/Button";
 import Axios from "axios";
 import Form from "react-bootstrap/Form";
 import { Link } from "react-router-dom";
 
-/*
-const submitTest = () => {
-  //react->server 요청을 보내고, 그 결과를 출력
-  Axios.get("http://localhost:8000/")
-    .then(function (response) {
-      // 성공 핸들링
-      alert("등록완료!");
-    })
-    .catch(function (error) {
-      // 에러 핸들링
-      console.log(error);
-    });
-};
-*/
-class Board extends Component {
-  render() {
-    return (
-      <tr>
-        <td>
-          <Form.Check // prettier-ignore
-            type="checkbox"
-            id={`default-checkbox`}
-            value={this.props.id}
-            onChange={(e) => {
-              this.props.onCheckboxChange(e.target.checked, e.target.value);
-            }}
-          />
-        </td>
-        <td>{this.props.id}</td>
-        <td>
-          <Link to={`/view?id=${this.props.id}`}>{this.props.title}</Link>
-        </td>
-        <td>{this.props.registerId}</td>
-        <td>{this.props.date}</td>
-      </tr>
-    );
-  }
+function Board({ id, title, registerId, date, onCheckboxChange }) {
+  return (
+    <tr>
+      <td>
+        <Form.Check // prettier-ignore
+          type="checkbox"
+          id={`default-checkbox`}
+          value={id}
+          onChange={(e) => {
+            onCheckboxChange(e.target.checked, e.target.value);
+          }}
+        />
+      </td>
+      <td>{id}</td>
+      <td>
+        <Link to={`/view/${id}`}>{title}</Link>
+      </td>
+      <td>{registerId}</td>
+      <td>{date}</td>
+    </tr>
+  );
 }
 
-export default class BoardList extends Component {
-  state = {
-    BoardList: [],
-    checkList: [],
-  };
-  onCheckboxChange = (checked, id) => {
-    const list = [...this.state.checkList];
-    if (checked) {
-      if (!list.includes(id)) {
-        list.push(id);
-      } else {
-        let idx = list.indexOf(id);
-        list.splice(idx, 1);
-      }
-    }
+const BoardList = ({ isComplete, handleModify }) => {
+  const [boardList, setBoardList] = useState([]);
+  const [checkList, setCheckList] = useState([]);
 
-    this.setState({
-      checkList: list,
+  const onCheckboxChange = (checked, id) => {
+    setCheckList((prevList) => {
+      //prevList는 checkList 기존값
+      if (checked) {
+        return [...prevList, id];
+      } else {
+        return prevList.filter((item) => item !== id);
+      }
     });
-    console.log(this.state.checkList);
   };
-  getList = () => {
+
+  const getList = useCallback(() => {
     Axios.get("http://localhost:8000/list")
       .then((res) => {
-        //const data = res.data;
         const { data } = res; //destructuring 비구조 할당
-        this.setState({
-          BoardList: data,
-        });
-        this.props.renderComplete(); //App.js에 목록 출력이 완료되었다고 전달
+        setBoardList(data);
       })
       .catch((e) => {
         // 에러 핸들링
         console.log(e);
       });
-  };
-  componentDidMount() {
-    this.getList();
-  }
-  componentDidUpdate(prevProps) {
-    // 수정모드이고 boardId가 변경되었다면, 그 글의 내용조회(detail 함수) 실행
-    if (this.props.isComplete !== prevProps.isComplete) {
-      this.getList();
+  }, []);
+
+  useEffect(() => {
+    getList();
+  }, [getList]); //최초 한번 getList 실행, getList객체가 변경되면 getList 실행
+
+  useEffect(() => {
+    if (isComplete) {
+      getList();
     }
-  }
-  handleDelete = () => {
-    if (this.state.checkList.length === 0) {
+  }, [isComplete]);
+
+  const handleDelete = () => {
+    if (checkList.length === 0) {
       alert("삭제할 게시글을 선택하세요");
       return;
     }
 
-    /*
-    let boardIDList = '';
-    this.state.checkList.forEach((num) => {
-      boardIDList = boardIDList + `${num},`;
-      console.log(boardIDList); // 1,2,3,
-      });
-      */
-    let boardIDList = this.state.checkList.join();
+    let boardIDList = checkList.join(); //1,2,3
 
     Axios.post("http://localhost:8000/delete", {
       boardIDList,
     })
       .then((res) => {
-        this.getList();
+        getList();
       })
       .catch((e) => {
         // 에러 핸들링
@@ -116,52 +85,50 @@ export default class BoardList extends Component {
       });
   };
 
-  render() {
-    console.log(this.props);
-    console.log(this.state.BoardList);
-    return (
-      <>
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>선택</th>
-              <th>번호</th>
-              <th>제목</th>
-              <th>작성자</th>
-              <th>작성일</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.BoardList.map((item) => (
-              <Board
-                key={item.BOARD_ID}
-                id={item.BOARD_ID}
-                title={item.BOARD_TITLE}
-                registerId={item.REGISTER_ID}
-                date={item.REGISTER_DATE}
-                onCheckboxChange={this.onCheckboxChange}
-              />
-            ))}
-          </tbody>
-        </Table>
-        <div className="d-flex gap-1">
-          <Link to="/write" className="btn btn-primary">
-            글쓰기
-          </Link>
+  return (
+    <>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>선택</th>
+            <th>번호</th>
+            <th>제목</th>
+            <th>작성자</th>
+            <th>작성일</th>
+          </tr>
+        </thead>
+        <tbody>
+          {boardList.map((item) => (
+            <Board
+              key={item.BOARD_ID}
+              id={item.BOARD_ID}
+              title={item.BOARD_TITLE}
+              registerId={item.REGISTER_ID}
+              date={item.REGISTER_DATE}
+              onCheckboxChange={onCheckboxChange}
+            />
+          ))}
+        </tbody>
+      </Table>
+      <div className="d-flex gap-1">
+        <Link to="/write" className="btn btn-primary">
+          글쓰기
+        </Link>
 
-          <Button
-            variant="secondary"
-            onClick={() => {
-              this.props.handleModify(this.state.checkList);
-            }}
-          >
-            수정하기
-          </Button>
-          <Button variant="danger" onClick={this.handleDelete}>
-            삭제하기
-          </Button>
-        </div>
-      </>
-    );
-  }
-}
+        <Button
+          variant="secondary"
+          onClick={() => {
+            handleModify(checkList);
+          }}
+        >
+          수정하기
+        </Button>
+        <Button variant="danger" onClick={handleDelete}>
+          삭제하기
+        </Button>
+      </div>
+    </>
+  );
+};
+
+export default BoardList;
